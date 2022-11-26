13a87dea013f13b576b8b1ea9da9b60dc94eeae3
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-11-26 21:22:12 +0300

    feature/creating_page_for_search     1.  Updating users projects and news pages

--- resources/views/public/user/news/index.blade.php
+++ resources/views/public/user/news/index.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.user.user')
 @section('title','Новости')
 @section('userContent')
+    @include('includes.searchForm')
     @if(count($news)==0)
-        <h3 class="badText">У пользователя {{$user->name}} нет новостей</h3>
+        @if(auth()->user()!==null && auth()->user()->name===Request::segment(2))
+            <h3 class="badText">
+                У Вас нет новостей
+            </h3>
+        @else
+            <h3 class="badText">
+                У пользователя {{$user->name}} нет новостей
+            </h3>
+        @endif
     @else
-        @include('includes.searchForm')
         @foreach($news as $newsCard)
             @include('includes.news.card')
         @endforeach
--- resources/views/public/user/projects/index.blade.php
+++ resources/views/public/user/projects/index.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.user.user')
 @section('title','Проекты')
 @section('userContent')
+    @include('includes.searchForm')
     @if(count($projects)==0)
-        <h3 class="badText">
-            У пользователя {{$user->name}} нет проектов
-        </h3>
+        @if(auth()->user()!==null && auth()->user()->name===Request::segment(2))
+            <h3 class="badText">
+                У Вас нет проектов
+            </h3>
+        @else
+            <h3 class="badText">
+                У пользователя {{$user->name}} нет проектов
+            </h3>
+        @endif
     @else
-        @include('includes.searchForm')
         @foreach($projects as $project)
             @include('includes.project.card')
         @endforeach
