8ca682652d828b3d1da90321a3958f163178c8cb
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-11-26 21:15:27 +0300

    feature/creating_page_for_search     1. Project search can use tags also automatically

--- app/Http/Controllers/Pages/ProjectsPageController.php
+++ app/Http/Controllers/Pages/ProjectsPageController.php
@@ -10,10 +10,20 @@ class ProjectsPageController extends Controller
     public function __invoke()
     {
         if (isset($_GET['query'])) {
-            $projects = $this->paginate(Project::where('title', 'like', '%' . $_GET['query'] . '%')->get(),10, '', ["path" => url()->current()]);
+            //Берём проекты по тегу
+            $projectTags = Project::whereHas('tags', function ($query) {
+                $rawTags = explode(' ', $_GET['query']);
+                $query->whereIn('name', $rawTags);
+            })->get();
+            //Берём проекты по названию
+            $projectsTitle = Project::where('title', 'like', '%' . $_GET['query'] . '%')->get();
+            //Объединяем проекты
+            $projectsAll = $projectsTitle->merge($projectTags);
+            //Ввыводим их
+            $projects = $this->paginate($projectsAll, 10, '', ["path" => url()->current()]);
             return view('public.search.indexProjects', compact('projects'));
         }
-        $projects = $this->paginate(Project::all(),10, '', ["path" => url()->current()]);
+        $projects = $this->paginate(Project::all(), 10, '', ["path" => url()->current()]);
         return view('public.search.indexProjects', compact('projects'));
     }
 }
--- app/Http/Controllers/User/Project/IndexController.php
+++ app/Http/Controllers/User/Project/IndexController.php
@@ -13,7 +13,18 @@ class IndexController extends Controller
         if (User::where('name', $username)->exists()) {
             $user = User::where('name', $username)->first();
             if (isset($_GET['query'])) {
-                $projects = $this->paginate(Project::where('user_id', $user->id)->where('title', 'like', '%' . $_GET['query'] . '%')->get(), 10, '', ["path" => url()->current()]);
+
+                //Берём проекты по тегу
+                $projectTags = Project::whereHas('tags', function ($query) {
+                    $rawTags = explode(' ', $_GET['query']);
+                    $query->whereIn('name', $rawTags);
+                })->where('user_id', $user->id)->get();
+                //Берём проекты по названию
+                $projectsTitle = Project::where('title', 'like', '%' . $_GET['query'] . '%')->where('user_id', $user->id)->get();
+                //Объединяем проекты
+                $projectsAll = $projectsTitle->merge($projectTags);
+                //Ввыводим их
+                $projects = $this->paginate($projectsAll, 10, '', ["path" => url()->current()]);
                 return view('public.user.projects.index', compact('user', 'projects'));
             }
             $projects = $this->paginate($user->projects, 10, '', ["path" => url()->current()]);
