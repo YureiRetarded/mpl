3a23a429f680964d99c4377344ea51d8663d0f92
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-11-28 00:46:51 +0300

    feature/updating_index_page_and_about_page     1. Updated pages

--- app/Http/Controllers/Pages/HomeController.php
+++ app/Http/Controllers/Pages/HomeController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
+use App\Models\News;
+use App\Models\Project;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('public.index');
+        function getSmallString($count): string
+        {
+            $string = '';
+            if ($count >= 1000 && $count < 1000000) {
+                if ($count % 1000 === 0) {
+                    $string = ($count / 1000);
+                } else {
+                    $string = substr($count, 0, -3) . '.' . substr($count, -3, -2);
+
+                    if (substr($string, -1, 1) === '0') {
+                        $string = substr($string, 0, -2);
+                    }
+                }
+                return $string .= 'k';
+            }
+            return $count >= 1000000 ? $string : $count;
+        }
+
+        $news = getSmallString(News::all()->count());
+        $projects = getSmallString(Project::all()->count());
+        $users = getSmallString(User::all()->count());
+
+        return view('public.index', compact('news', 'projects', 'users'));
     }
 }
