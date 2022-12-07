6d6094f822f7cf8c3e5dfd5b7f850fb50b058f4a
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-12-07 21:55:33 +0300

    origin/main hot fix     1. Changed template     2. Chaged regix for user login

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">Подтвердите свой email</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            Новый ссылка была отправлена
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    Пожалуйста подтвердите свой email, прежде чем продолжить.
+                    Если вы не получили ссылку:
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">Нажмите чтобы отправить новую ссылку</button>.
                     </form>
                 </div>
             </div>
