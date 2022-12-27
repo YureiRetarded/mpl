bab9e19f8f8cdb00995152532d5e67f07b51beb7
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-12-27 19:09:56 +0300

    feature/updating_setting     1. User card updated for admin

--- resources/views/includes/user/card.blade.php
+++ resources/views/includes/user/card.blade.php
@@ -1,10 +1,3 @@
-{{--<div class="container text-center card mb-3 p-1 ">--}}
-{{--    <div role="button" class="row  " onclick="location.href='{{route('user.index',['user'=>$user->link])}}'">--}}
-{{--        <div class="col"> {{$user->name}}({{$user->link}})</div>--}}
-{{--        <div class="col"> {{__('messages.projects')}}: {{$user->projects->count()}}</div>--}}
-{{--        <div class="col"> {{__('messages.posts')}}: {{$user->posts->count()}}</div>--}}
-{{--    </div>--}}
-{{--</div>--}}
 <div class="card mb-3 " role="button" onclick="location.href='{{route('user.index',['user'=>$user->link])}}'">
     <div class="row g-0">
         <div class="col-md-4">
--- resources/views/includes/user/cardAdmin.blade.php
+++ resources/views/includes/user/cardAdmin.blade.php
@@ -1,14 +1,27 @@
-<div class="container text-center card mb-3 p-1 ">
-    <div role="button" class="row">
-        <div class="col"> {{$user->name}}</div>
-        <div class="col"> {{__('messages.projects')}}: {{$user->projects->count()}}</div>
-        <div class="col"> {{__('messages.posts')}}: {{$user->posts->count()}}</div>
-        <div class="col"> {{__('messages.role')}}: {{$user->role->name}}</div>
-        <div class="col">
-            <a class="btn btn-primary"
-               href="{{route('admin.user.edit',['user'=>$user->id])}}">{{__('messages.edit')}}</a>
-            <a target="_blank" class="btn btn-primary"
-               href="{{route('user.index',['user'=>$user->link])}}">{{__('messages.open')}}</a>
+<div class="card mb-3 " role="button" onclick="location.href='{{route('user.index',['user'=>$user->link])}}'">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img
+                src="{{ $user->avatar != '' && Storage::exists('public/' . $user->avatar) ? asset('/storage/'.$user->avatar) : asset('/storage/no_image.png') }}"
+                class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h3 class="card-title">{{$user->name}}</h3>
+                <h4 class="card-text">{{__('messages.projects')}}: <a
+                        class="no-underline"
+                        href="/users/{{$user->link}}/projects">{{$user->projects->count()}}</a></h4>
+                <h4 class="card-text">{{__('messages.posts')}}: <a
+                        class="no-underline"
+                        href="/users/{{$user->link}}/posts">{{$user->posts->count()}}</a></h4>
+                <p class="card-text">{{isset($user->about)?mb_strimwidth($user->about,0,500,'...'):$user->name.' '.__('messages.no_user_description')}}</p>
+                <div class="card-text">
+                    <a class="btn btn-primary"
+                       href="{{route('admin.user.edit',['user'=>$user->id])}}">{{__('messages.edit')}}</a>
+                    <a target="_blank" class="btn btn-primary"
+                       href="{{route('user.index',['user'=>$user->link])}}">{{__('messages.open')}}</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
